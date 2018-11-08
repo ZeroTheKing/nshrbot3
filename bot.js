0359a3ab08833cdc6058b7a7dc72084a51722211
ZeroTheKing <44266197+ZeroTheKing@users.noreply.github.com>
2018-11-08 14:18:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,13 @@ console.log("Welcome Again !");
  client.on("guildMemberRemove", member => {
   member.createDM().then(function (channel) {
   return channel.send(`!!
-الحق نظام انفايت رهيب مرا :money_mouth: 
-6 Invites= 15K
-:heart_exclamation:  https://discord.gg/WRUUc42 :heart_exclamation:`)
+حياك ربي اعرف ما تقصر
+
+ممكن تصوتلي رقم 25
+
+وشكرا لك ي بعدي :heart_exclamation:
+
+https://discord.gg/NPZFQgN`)
 }).catch(console.error)
 
 })
@@ -16,9 +20,13 @@ console.log("Welcome Again !");
 client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(`!!
-الحق نظام انفايت رهيب مرا :money_mouth: 
-6 Invites= 15K
-:heart_exclamation:  https://discord.gg/WRUUc42 :heart_exclamation:`)
+حياك ربي اعرف ما تقصر
+
+ممكن تصوتلي رقم 25
+
+وشكرا لك ي بعدي :heart_exclamation:
+
+https://discord.gg/NPZFQgN`)
 }).catch(console.error)
 })
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
